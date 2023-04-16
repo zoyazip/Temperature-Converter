ad95ceca664226e006fb4116fd3c0db80321cc33
Denis Chernov <chernoff.cs@gmail.com>
2023-04-16 11:30:54 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by d.chernov on 16/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
